384ffe4cf43d43830b88c825efa846be2b6b2c33
ZPyrolink <corenthin.zozor@gmail.com>
2024-04-10 10:01:54 +0200

    Update lombok version and remove unused class

--- src/main/java/fr/uga_ctj/panda/Main.java
+++ src/main/java/fr/uga_ctj/panda/Main.java
@@ -1,9 +1,8 @@
 package fr.uga_ctj.panda;
-public class Main{
 
-    public static void main(String[] args)  {
+public class Main {
 
-        Panda test = new Panda();
+    public static void main(String[] args) {
         System.out.println("hey");
     }
 
