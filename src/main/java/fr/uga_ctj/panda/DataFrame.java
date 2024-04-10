7bd1fe173d8d6be5661df9cb1a478a0c68d05b5f
745th <theolorgnier@outlook.fr>
2024-04-10 11:15:42 +0200

    Constructor

--- src/main/java/fr/uga_ctj/panda/DataFrame.java
+++ src/main/java/fr/uga_ctj/panda/DataFrame.java
@@ -4,19 +4,67 @@ import lombok.Getter;
 
 import java.io.FileInputStream;
 import java.util.Map;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class DataFrame {
     static class Obj<T> {
         @Getter
         private T value;
+
+        public Obj(T value) {
+            this.value = value;
+        }
     }
 
-    private Map<String, Obj[]> map;
+    private Map<String, Obj[]> Data;
 
     //region Ctor
-
-    public DataFrame(String[] label, Object[] values) {
-
+    //x and y are switch
+    public DataFrame(String[] label, Object[][] values) {
+        Obj[] currentCol = new Obj[values[0].length];
+        Data = new HashMap<>();
+        for(int i=0;i<label.length;i++)
+        {
+            currentCol = new Obj[values[i].length];
+            
+            for(int j=0;j<values[i].length;j++)
+            {
+                Obj obj;
+                if(values[i][0] instanceof Integer)
+                {
+                    obj = new Obj<>((Integer) values[i][j]);
+                }
+                else if(values[i][0] instanceof Double)
+                {
+                    obj = new Obj<>((Double) values[i][j]);
+                }
+                else if(values[i][0] instanceof Float)
+                {
+                    obj = new Obj<>((Float) values[i][j]);
+                }
+                else if(values[i][0] instanceof Character)
+                {
+                    obj = new Obj<>((Character) values[i][j]);
+                }
+                else if(values[i][0] instanceof Long)
+                {
+                    obj = new Obj<>((Long) values[i][j]);    
+                }
+                else if(values[i][0] instanceof Short)
+                {
+                    obj = new Obj<>((Short) values[i][j]);    
+                }
+                else
+                {
+                    throw new RuntimeException();
+                }
+                
+                currentCol[j]=obj;                
+            }
+            Data.put(label[i],currentCol);
+        }
+            
     }
 
     public DataFrame(FileInputStream stream) {
